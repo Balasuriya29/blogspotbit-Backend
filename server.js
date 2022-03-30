1ebaafcf38b6426826ab5341237a2ef490e0f58f
balasuriya <balasuriya.cs20@bitsathy.ac.in>
2022-03-30 21:05:33 +0530

    test43

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ if(!config.get('jwtPrivateKey')){
     console.error('FATAL ERROR: jwtPrivateKey is not defined');
     process.exit(1);
 }
-
 const app = express();
 app.use(express.json());
 app.use('/api/blogs', blogs);
